04863a7adcac09fa36f91c1caaf6fd1d2342c717
EstebanKuh <Esteban.kuh@gmail.com>
2021-11-23 20:20:25 -0600

    test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -4,4 +4,18 @@ class ArticlesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "index exitoso" do
+    get articles_path
+    assert_response :success
+  end
+
+  test "POST crear: Crear un artículo" do
+    assert_difference 'Article.count', 1 do
+      post articles_path, params: {article: {title:"TestTitulo", body: "TestText"}}
+    end
+
+    assert_redirected_to article_url(Article.last)
+  end
+
 end
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -4,4 +4,20 @@ class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  test "Creación artículo sin título" do
+    article = Article.new
+    assert_not article.save
+  end
+
+  test "Artículos publicados" do
+    articles = Article.published
+    assert_equal 1, articles.length
+
+    assert_equal articles(:publicados).id, articles[2].id
+  end
+
+  test "Título Ruby" do
+    assert_equal "RubyTitle", articles(:ruby).title
+  end
 end
